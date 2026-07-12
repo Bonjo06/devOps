1ff2f7ada16598d18c76bca777947cd036c979cc
Benjamin Alejandro Mella Cordero <ben.mellac@duocuc.cl>
2026-07-12 02:14:31 -0400

    fix ventas

--- back-Ventas_SpringBoot/Springboot-API-REST/src/main/java/com/citt/config/OpenApiConfing.java
+++ back-Ventas_SpringBoot/Springboot-API-REST/src/main/java/com/citt/config/OpenApiConfing.java
@@ -4,7 +4,7 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@Configuration   
 @OpenAPIDefinition(      
         info = @Info(
                 title = "API REST Ventas",
